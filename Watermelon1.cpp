cef8a2048310d73aa0de6a1a8d50f30f003fb953
ZeyadGouda <135771697+ZeyadGouda@users.noreply.github.com>
2026-01-09 16:29:36 +0200

    Refactor Watermelon1.cpp for clarity and efficiency

--- Watermelon1.cpp
+++ Watermelon1.cpp
@@ -1,47 +1,144 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
+void FIO()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+void testcases()
+{
+	double n;
+	cin >> n;
+	int r, p, s;
+	cin >> r >> p >> s;
+	string play;
+	cin >> play;
+	ll points = 0;
+	int r1 = r, p1 = p, s1 = s;
+	for (int i = 0; i < play.size(); i++)
+	{
+		if (play[i] == 'R')
+		{
+			if (p1 > 0)
+			{
+				points++;
+				p1--;
+			}
+			else if (r1 > 0)
+			{
+				r1--;
+			}
+			else if (s1 > 0)
+				s1--;
+		}
+		if (play[i] == 'P')
+		{
+			if (s1 > 0)
+			{
+				points++;
+				s1--;
+			}
+			else if (p1 > 0)
+			{
+				p1--;
+			}
+			else if (r1 > 0)
+				r1--;
+		}
+		if (play[i] == 'S')
+		{
+			if (r1 > 0)
+			{
+				points++;
+				r1--;
+			}
+			else if (s1 > 0)
+			{
+				s1--;
+			}
+			else if (p1 > 0)
+				p1--;
+		}
+	}
+
+	p1 = p, r1 = r, s1 = s;
+
+	if (points >= ceil(n / 2))
+	{
+		cout << "YES\n";
+		for (int i = 0; i < play.size(); i++)
+		{
+			if (play[i] == 'R')
+			{
+				if (p1 > 0)
+				{
+					cout << 'P';
+					p1--;
+				}
+				else if (r1 > 0)
+				{
+					cout << 'R';
+					r1--;
+				}
+				else if (s1 > 0)
+				{
+					s1--;
+					cout << 'S';
+				}
+			}
+			if (play[i] == 'P')
+			{
+				if (s1 > 0)
+				{
+					cout << 'S';
+					s1--;
+				}
+				else if (p1 > 0)
+				{
+					cout << 'P';
+					p1--;
+				}
+				else if (r1 > 0)
+				{
+					cout << 'R';
+					r1--;
+				}
+			}
+			if (play[i] == 'S')
+			{
+				if (r1 > 0)
+				{
+					cout << 'R';
+					r1--;
+				}
+				else if (s1 > 0)
+				{
+					cout << "S";
+					s1--;
+				}
+				else if (p1 > 0)
+				{
+					cout << 'P';
+					p1--;
+				}
+			}
+		}
+		cout << '\n';
+	}
+	else
+		cout << "NO\n";
+}
+
 int main()
 {
-	long long id; cin >> id;
-	bool pal = false;
-	long long rev = 0,checkpoint = id, digits = 0;
-	while (id != 0)
-	{
-		digits +=1;
-		id /= 10;
-	}
-	id = checkpoint;
-	for (int j = 1; j <= digits; j++)
-	{
-		if (j == 1)	rev = rev + (id % 10);
-		else rev = rev*10 + (id % 10);
-		id = id / 10;
-		if (id == rev)
-		{
-			pal = true;
-			break;
-		}
-	}
-	id = checkpoint;
-	long long sum = 0;
-	for (long long i = 1; i <= digits; i++)
-	{
-		if (id != 0)
-		{
-			long long x = id % 10;
-			id /= 10;
-			sum = sum + x;
-		}
-	}
-	cout << sum << endl;
-	cout << pal << endl;
-	if (id % sum == 0 && pal == true)
-		cout << "Best mentor.";
-	else if (id % sum != 0 && pal == true)
-		cout << "He's good.";
-	else if (id % sum == 0 && pal == false)
-		cout << "He's good.";
-	else
-		cout << "He'll be fired.";
-		return 0;
+	FIO();
+	int tests = 1;
+	cin >> tests;
+	while (tests--)
+		testcases();
+	return 0;
 }
