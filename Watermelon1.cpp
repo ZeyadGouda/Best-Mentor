5398f79598f8d1ac87e46802e40e7bedb35ae8e3
ZeyadGouda <135771697+ZeyadGouda@users.noreply.github.com>
2026-07-09 21:15:07 +0300

    C - Bored Judge

--- Watermelon1.cpp
+++ Watermelon1.cpp
@@ -1,100 +1,109 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 using ll = long long;
+using ld = long double;
 ll M = 1e9 + 7;
+#define oset tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define omset tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>
 
 void FIO()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-}
-
-vector<vector<ll>> sparmx, sparmn;
-
-ll mn(ll l, ll r)
-{
-    ll pw = log2(r - l + 1);
-    return min(sparmn[pw][l], sparmn[pw][r - (1 << pw) + 1]);
-}
-ll mx(ll l, ll r)
-{
-    ll pw = log2(r - l + 1);
-    return max(sparmx[pw][l], sparmx[pw][r - (1 << pw) + 1]);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 }
 
 void testcases()
 {
-    ll n;
-    cin >> n;
-    ll lg = log2(n) + 1;
-    sparmx.resize(lg, vector<ll>(n));
-    sparmn.resize(lg, vector<ll>(n));
-    for (ll i = 0; i < n; i++)
-        cin >> sparmx[0][i];
-    for (ll pw = 1; pw < lg; pw++)
-        for (ll i = 0; i + (1 << pw) <= n; i++)
-            sparmx[pw][i] = max(sparmx[pw - 1][i], sparmx[pw - 1][i + (1 << (pw - 1))]);
+	ll n, m;
+	cin >> n >> m;
+	vector<pair<ll, ll>> v(m);
+	for (ll i = 0; i < m; i++)
+		cin >> v[i].first >> v[i].second;
 
-    for (ll i = 0; i < n; i++)
-        cin >> sparmn[0][i];
-    for (ll pw = 1; pw < lg; pw++)
-        for (ll i = 0; i + (1 << pw) <= n; i++)
-            sparmn[pw][i] = min(sparmn[pw - 1][i], sparmn[pw - 1][i + (1 << (pw - 1))]);
-// DONE creating the sparse tabels
-    ll ans = 0;
-    for (ll l = 0; l < n; l++) // for each possible number
-    {
-        ll start = l, end = n - 1, left = -1;
-        while (start <= end) // binary search for the first left bound that has min = max
-        {
-            ll mid = (start + end) / 2;
-            ll a = mx(l, mid); // Getting the answer from the sparse
-            ll b = mn(l, mid); // Getting the answer from the sparse
-            if (a == b)
-            {
-                end = mid - 1;
-                left = mid;
-            }
-            else if (a > b)
-                end = mid - 1;
-            else if (b > a)
-                start = mid + 1;
-        }
-        if (!(left + 1))
-            continue;
-        ll right = -1;
-        start = left, end = n - 1;
+	vector<ll> ans, points(n + 1, 0);
 
-        while (start <= end) // binary search for the right bound where the streak of min = max ends
-        {
-            ll mid = (start + end) / 2;
-            ll a = mx(l, mid);
-            ll b = mn(l, mid);
-            if (a == b)
-            {
-                start = mid + 1;
-                right = mid;
-            }
-            else if (a > b)
-                end = mid - 1;
-            else
-                start = mid + 1;
-        }
-        if (left + 1 and right + 1)
-            ans += right - left + 1; // add length if found right and left boundries
-    }
-    cout << ans << '\n';
+	// Set 1st input as Default Leading Team
+	ll LeadingTeam = v[0].first;
+	points[LeadingTeam] = v[0].second;
+	ans.push_back(LeadingTeam);
+
+	set<pair<ll, ll>> st;
+	st.insert({-v[0].second, v[0].first}); // Sorting Dessinding according to points, Realiesed now that there was no need for it to be Dessinding
+
+	for (ll i = 1; i < m; i++)
+	{
+		ll curTeam = v[i].first, curPoints = v[i].second;
+		pair<ll, ll> add = {-points[curTeam], curTeam};
+		if (st.count(add)) // if the team exists remove it -> edit the points and re insert it
+		{
+			st.erase(add);
+			points[curTeam] += curPoints;
+			add.first = (-curPoints);
+			st.insert(add);
+		}
+		else // else just insert it
+			st.insert({-curPoints, curTeam});
+
+		bool isLeadingTeamUpdated = 0;
+
+		if (points[curTeam] + curPoints > points[LeadingTeam]) // added +ve points and exceeded the Leading team -> update it
+			LeadingTeam = curTeam;
+
+		else if (points[curTeam] + curPoints == points[LeadingTeam]) // added points and 2 teams are Leading -> take the min(IDs)
+			LeadingTeam = min(LeadingTeam, curTeam);
+
+		else if (curTeam == LeadingTeam and curPoints < 0) // added -ve points and the Leading team dropped in rank -> second Leading becomes Leading
+		{
+			pair<ll, ll> y = {-points[LeadingTeam], LeadingTeam};
+			st.erase(y);
+			points[LeadingTeam] += curPoints;
+			st.insert({-points[LeadingTeam], LeadingTeam});
+			auto it = st.begin(); // st.begin() after removing Leading : AKA Second Leading -> becomes Leading
+			pair<ll, ll> z = (*it);
+			LeadingTeam = z.second;
+			isLeadingTeamUpdated = true; // To Avoid updating it twice
+		}
+
+		pair<ll, ll> x = {-points[curTeam], curTeam};
+
+		if (st.count(x) != 0) // if curTeam already in set -> remove it
+			st.erase(x);
+
+		if (!isLeadingTeamUpdated) // for all exept Leading -> Update points and re insert the team with new points
+		{
+			points[curTeam] += curPoints;
+			st.insert({-points[curTeam], curTeam});
+		}
+		ans.push_back(LeadingTeam); // Pushing the leading Team -> (idx + 1 ,1-based) is the round number that has (LeadingTeam) as 1st Team
+	}
+
+	reverse(ans.begin(), ans.end());
+
+	ll i = 1;
+	for (; i < m; i++)
+	{
+		if (ans[i] != ans[i - 1])
+			break;
+	}
+	// Checking last run where did it start
+	ll f = m - i + 1;
+
+	if (f == 1) // if it started on the 1st day and it is the only Leading Team for all Days cout << 0
+		cout << 0 << '\n';
+	else // else cout The Day the last streak Started
+		cout << m - i + 1 << '\n';
 }
-// RUNTIME ERROR DUE TO INDEXING -> I've no clue which one is it or why
 
 int main()
 {
-    FIO();
-    ll tests = 1;
-    cin >> tests;
-    while (tests--)
-        testcases();
-    return 0;
+	FIO();
+	ll tests = 1;
+	cin >> tests;
+	while (tests--)
+		testcases();
+	return 0;
 }
-
